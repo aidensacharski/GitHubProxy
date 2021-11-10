9a625b436a4031a709df3e7c1edb2963b3b002c8
yigolden <jinyi@live.cn>
2021-11-10 09:59:14 +0800

    Update publish.bat

--- publish.bat
+++ publish.bat
@@ -12,9 +12,17 @@ REM win-x64 self contained
 dotnet publish -c Release -r win-x64 --self-contained=true -p:PublishTrimmed=true -p:PublishSingleFile=true -o bin/artifacts/win-x64-self-contained
 del bin\artifacts\win-x64-self-contained\appsettings.Development.json
 
+REM win-arm64 self contained
+dotnet publish -c Release -r win-arm64 --self-contained=true -p:PublishTrimmed=true -p:PublishSingleFile=true -o bin/artifacts/win-arm64-self-contained
+del bin\artifacts\win-arm64-self-contained\appsettings.Development.json
+
 REM linux-x64 self contained
 dotnet publish -c Release -r linux-x64 --self-contained=true -p:PublishTrimmed=true -p:PublishSingleFile=true -o bin/artifacts/linux-x64-self-contained
 del bin\artifacts\linux-x64-self-contained\appsettings.Development.json
+
+REM linux-arm64 self contained
+dotnet publish -c Release -r linux-arm64 --self-contained=true -p:PublishTrimmed=true -p:PublishSingleFile=true -o bin/artifacts/linux-arm64-self-contained
+del bin\artifacts\linux-arm64-self-contained\appsettings.Development.json
 
 REM osx-x64
 dotnet publish -c Release -r osx-x64 --self-contained=true -p:PublishTrimmed=true -p:PublishSingleFile=true -o bin/artifacts/osx-x64-self-contained
